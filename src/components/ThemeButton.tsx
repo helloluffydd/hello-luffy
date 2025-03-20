66faddc04a89d833a0d75797364b1b3def7a12c4
Luffy Chen <luffychen0715@gmail.com>
2025-03-20 23:18:45 +0800

    fix(ThemeButton): dark mode toggling

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -4,31 +4,33 @@ import { MoonStar, Sun } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 export function ThemeButton() {
-  const [theme, setTheme] = useState('');
+  const [theme, setTheme] = useState('light');
 
   useEffect(() => {
-    const theme = window.localStorage.getItem('theme');
-    if (theme) {
-      setTheme(theme);
-    }
+    const theme = window.localStorage.getItem('theme') || 'light';
+    setTheme(theme);
   }, []);
 
-  useEffect(() => {
-    window.localStorage.setItem('theme', theme);
-    document.documentElement.classList.toggle(
-      'dark',
-      window.localStorage.theme === 'dark' ||
-        (!('theme' in window.localStorage) &&
-          window.matchMedia('(prefers-color-scheme: dark)').matches)
-    );
-  }, [theme]);
+useEffect(() => {
+  // Toggle the 'dark' class on the document's root element based on the theme state.
+  // If the theme is 'dark', or if the theme is not set in localStorage and the user prefers a dark color scheme,
+  // the 'dark' class will be applied.
+  document.documentElement.classList.toggle(
+    'dark',
+    theme === 'dark' ||
+    (!('theme' in window.localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+  );
+}, [theme]);
+  
+  const onClickThemeButton = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    window.localStorage.setItem('theme', nextTheme);
+    setTheme(nextTheme);
+  };
 
   return (
-    <button
-      type="button"
-      className="w-8"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-    >
+    <button type="button" className="w-8" onClick={onClickThemeButton}>
       {theme === 'light' && <Sun />}
       {theme === 'dark' && <MoonStar />}
     </button>

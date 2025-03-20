48962c80305c8e600b27cce2a2d153261c38aee7
Luffy Chen <luffychen0715@gmail.com>
2025-03-20 23:19:48 +0800

    fix(PostCard): use Image from Next.js

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface PostCardProps {
   title: string;
+  url: string;
   publishedAt: string;
   excerpt: string;
   coverImgAlt: string;
   coverImgSrc: string;
-  url: string;
 }
 
 const PostCard: React.FC<PostCardProps> = ({
@@ -18,20 +19,17 @@ const PostCard: React.FC<PostCardProps> = ({
   url,
 }) => {
   return (
-    <Link
-      className="flex flex-col space-y-1 mb-4 border border-solid border-neutral-900 overflow-hidden rounded-md dark:border-slate-200"
-      href={url}
-    >
-      <div>
-        <img
-          className="w-full h-56 object-cover object-center"
+    <Link className="flex flex-col space-y-1 mb-4 overflow-hidden " href={url}>
+      <span className="w-full inline-block relative aspect-4/3 rounded-md overflow-hidden">
+        <Image
           src={coverImgSrc}
           alt={coverImgAlt}
-          loading="lazy"
+          fill={true}
+          className="object-cover"
         />
-      </div>
+      </span>
       <div className="px-6 pt-4 pb-6 text-center">
-        <h2 className="text-xl font-semibold">{title}</h2>
+        <h2 className="text-xl font-semibold text-balance">{title}</h2>
         <div className="mt-2 text-xs text-gray-400">{publishedAt}</div>
         <div className="mt-2 text-sm text-justify">{excerpt}</div>
       </div>
